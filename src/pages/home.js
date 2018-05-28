333186ea3a721d3e6f425930d36bc6613ac7bc51
Alvin Go <alvingogo25@gmail.com>
2018-05-28 01:32:15 -0700

    started home page

---
+++ src/pages/home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default () => (
+  <div className="row">
+    <div className="col s12 m10">
+      <div className="section">
+        <h2>Alvin Go</h2>
+        <p className="flow-text">
+          I am Full Stack Web developer. I recently graduated from UCLA
+          Extension Coding Bootcamp and am ready for a career change into web development. 
+        </p>
+      </div>
+    </div>
+  </div>
+);
