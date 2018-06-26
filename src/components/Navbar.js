84955e7eab72f013f4d95addfab576407c43e602
Alvin Go <alvingogo25@gmail.com>
2018-06-26 00:33:58 -0700

    added hover for description on projects page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,43 @@
 import React from "react";
 import NavButton from "./NavButton";
 
-const style={
-    position: "relative",
-    display: "flex",
-    alignItems: 'flex-end',
-    justifyContent: 'space-around'
-}
+const style = {
+  position: "relative",
+  display: "flex",
+  alignItems: "flex-end",
+  justifyContent: "space-around"
+};
 
 class Navbar extends React.Component {
   render() {
     return (
-      // <div style={style} className="container">
-      //   <NavButton to={"/"} title={"Home"} />
-      //   <NavButton to={"/projects"} title={"Projects"} />
-      //   <NavButton to={"/contact"} title={"Contact"} />
-      // </div>
       <div>
-      <nav>
-        <div className="container">
-          <a href="#!" data-target="mobile" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+        <nav>
+          <div className="container">
+            <a href="#!" data-target="mobile" className="sidenav-trigger">
+              <i className="material-icons">menu</i>
+            </a>
             <ul style={style} className="hide-on-med-and-down">
-            <li><NavButton to={"/"} title={"Home"} /></li>
-            <li><NavButton to={"/projects"} title={"Projects"} /></li>
-            <NavButton to={"/contact"} title={"Contact"} />
-            
-          </ul>
-        </div>
-      </nav>
+              <li>
+                <NavButton to={"/"} title={"Home"} />
+              </li>
+              <li>
+                <NavButton to={"/projects"} title={"Projects"} />
+              </li>
+              <NavButton to={"/contact"} title={"Contact"} />
+            </ul>
+          </div>
+        </nav>
 
-      <ul className="sidenav" id="mobile">
-        <li><NavButton to={"/"} title={"Home"} /></li>
-        <li><NavButton to={"/projects"} title={"Projects"} /></li>
-        <NavButton to={"/contact"} title={"Contact"} />
-        
-      </ul>
+        <ul className="sidenav" id="mobile">
+          <li>
+            <NavButton to={"/"} title={"Home"} />
+          </li>
+          <li>
+            <NavButton to={"/projects"} title={"Projects"} />
+          </li>
+          <NavButton to={"/contact"} title={"Contact"} />
+        </ul>
       </div>
     );
   }
