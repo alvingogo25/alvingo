09048692131509ed44e2f230e853a805c6d0a89e
Alvin Go <alvingogo25@gmail.com>
2018-08-02 23:23:39 -0700

    added resume link

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,12 +3,29 @@ import ContactCard from "./../components/ContactCard";
 
 export default () => (
   <div>
-    <ContactCard link="https://www.github.com/alvingogo25" icon="github" label="Github" />
+    <ContactCard
+      link="https://www.github.com/alvingogo25"
+      icon="github"
+      label="Github"
+    />
 
-    <ContactCard link="https://www.linkedin.com/in/alvin-go" icon="linkedin" label="LinkedIn" />
+    <ContactCard
+      link="https://www.linkedin.com/in/alvin-go"
+      icon="linkedin"
+      label="LinkedIn"
+    />
 
-    <ContactCard link="mailto:alvingogo25@gmail.com" icon="mail" label="Email" />
+    <ContactCard
+      link="mailto:alvingogo25@gmail.com"
+      icon="mail"
+      label="Email"
+    />
 
-    <ContactCard link="#" icon="file" label="Resume" />
+    <ContactCard
+      link="https://drive.google.com/open?id=1UKc15-EXYTh9D8Joj4CjXE6tpjSOhkwe"
+      icon="file"
+      label="Resume"
+      download="true"
+    />
   </div>
 );
